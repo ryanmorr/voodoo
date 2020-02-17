9d49f313148a106ec0be0385f4a959015e30cd31
Ryan Morr <rm.morr@gmail.com>
2020-02-17 15:25:30 -0500

    Add test case for deleted variables

--- test/specs/voodoo.js
+++ test/specs/voodoo.js
@@ -3,13 +3,17 @@ import sinon from 'sinon';
 import voodoo from '../../src/voodoo';
 
 describe('voodoo', () => {
+    afterEach(() => {
+        delete global._val;
+    });
+
     it('should return a function', () => {
         const exec = voodoo('');
 
         expect(exec).to.a('function');
     });
 
-    it('should get a property value', () => {
+    it('should get the value of a variable', () => {
         const exec = voodoo(`
             global._val = foo;
         `);
@@ -18,7 +22,7 @@ describe('voodoo', () => {
         expect(global._val).to.equal(1);
     });
 
-    it('should set a property value', () => {
+    it('should set the value of a variable', () => {
         const exec = voodoo(`
             foo = 2;
             global._val = foo;
@@ -26,5 +30,21 @@ describe('voodoo', () => {
 
         exec({foo: 1});
         expect(global._val).to.equal(2);
+    });
+
+    it('should delete a variable', () => {
+        const exec = voodoo(`
+            if (delete foo) {
+                try {
+                    global._val = foo;
+                } catch (e) {
+                    global._val = e;
+                } 
+            }
+        `);
+
+        exec({foo: 1});
+        expect(global._val).to.be.an.instanceof(ReferenceError);
+        expect(global._val.message).to.equal('foo is not defined');
     });
 });
